8170619667a2079d49b2fa3ab0b9e91ec0c95836
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-10-28 15:34:30 +0000

    solved challenge 4

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -75,9 +75,14 @@ HR has asked you to change the data to make it easier to print so that it looks 
 
 const updateNumbers = (obj) => {
   // Solution code here...
-  let entries = [];
-  // entries.push(Object.entries(obj));
-  return entries.push(Object.entries(obj));
+  // let keys = Object.keys(obj);
+  // let values = Object.values(obj);
+  let newArr = [];
+
+  for (let [key, value] of Object.entries(obj)) {
+    newArr.push(`${key}: ${value}`);
+  }
+  return newArr;
 };
 
 
